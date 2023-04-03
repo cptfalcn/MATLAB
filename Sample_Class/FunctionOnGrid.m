6a7f170c4087c1ee68eec2668e3c188a94bf9056
cptfalcn <54957544+cptfalcn@users.noreply.github.com>
2023-04-03 11:30:54 -0700

    Update FunctionOnGrid.m
    Finalized the lesson

--- Sample_Class/FunctionOnGrid.m
+++ Sample_Class/FunctionOnGrid.m
@@ -1,14 +1,18 @@
 classdef FunctionOnGrid
-    %We will use the Sample Anonymous function files to organize them into
-    %a class of objects and methods.
+    %We will use the Sample Anonymous function files in a class
     %======================================================================
     %These are the 'properties', data that persists as long as the object
-    %exists, that is when it is 'cleared'
-    properties (Access=private)
+    %exists.  When you exit or until when it is 'cleared'
+    properties (Access=private) 
+    %======================================================================
     %Don't let your users touch your data, they will break things.
-    %Instead make them use your set functions.
-        Grid
-        Function
+    %Instead make them use your set functions with private attribute.
+    %https://www.mathworks.com/help/matlab/matlab_oop/property-attributes.html
+    %That is a full list of property attributes.
+    %The basic versions are public and private.
+    %======================================================================
+        Grid        %A Grid to evaluate your function over
+        Function    %A function handle @(x), evaluate on GRID
     end
     %----------------------------------------------------------------------
     methods  %These are the things the object can do.
@@ -23,6 +27,12 @@ classdef FunctionOnGrid
         function obj = SetGrid (obj, InGrid)
             obj.Grid= InGrid;
         end
+
+        %Perhaps you want to reset the grid.
+        function obj = SetFunction (obj, InFunction)
+            obj.Function= InFunction;
+        end
+
         %Methods callable on the matlab terminal via obj.method
         function PlotGrid(obj)
             figure()
